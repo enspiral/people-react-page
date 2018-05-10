44c50322818876dc2e5d0109292971e5b5091889
Daniel Lewis <dan@rootsystems.nz>
2018-05-10 18:01:54 +1200

    check for the enviroment variable

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+process.env.PERSON_API_URL ? console.info('Api url: ', process.env.PERSON_API_URL) : console.error('Enviroment Variable PERSON_API_URL not defined!')
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
