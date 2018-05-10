3f8c102ef4854872202724a86d9274f847ad577c
Daniel Lewis <dan@rootsystems.nz>
2018-05-10 18:37:19 +1200

    convert to es6 for consistancy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-require('dotenv').config()
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 process.env.REACT_APP_PERSON_API_URL ? console.info('Api url: ', process.env.REACT_APP_PERSON_API_URL) : console.error('Enviroment Variable REACT_APP_PERSON_API_URL not defined!')
 
