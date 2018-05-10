f5f93b6c0f6b30ff7f3998e6e69e54a7391a51a1
dazyang <dazyang@protonmail.com>
2018-05-10 20:58:55 +1200

    change stateful to funtional

--- src/components/ProfilesGrid.jsx
+++ src/components/ProfilesGrid.jsx
@@ -8,22 +8,20 @@ const masonryOptions = {
 };
 const imagesLoadedOptions = { background: '.my-bg-image-el' }
 
-class ProfilesGrid extends Component {
-  render() {
-    const { people } = this.props
-    return (
-      <Masonry
-        className={'my-gallery-class'} // default ''
-        elementType={'ul'} // default 'div'
-        options={masonryOptions} // default {}
-        disableImagesLoaded={false} // default false
-        updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
-        imagesLoadedOptions={imagesLoadedOptions} // default {}
-        >
-        { people.map((item) => {return <ProfileCard person={item}/>})}
-        </Masonry>
-    )
-  }
+function ProfilesGrid (props) {
+  const { people } = props
+  return (
+    <Masonry
+      className={'my-gallery-class'} // default ''
+      elementType={'ul'} // default 'div'
+      options={masonryOptions} // default {}
+      disableImagesLoaded={false} // default false
+      updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+      imagesLoadedOptions={imagesLoadedOptions} // default {}
+      >
+      { people.map((item) => {return <ProfileCard person={item}/>})}
+      </Masonry>
+  )
 }
 
 export default ProfilesGrid;
