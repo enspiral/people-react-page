9664d300a6f7d70c9f42a212218f1d0283f9e47c
dazyang <dazyang@protonmail.com>
2018-05-16 10:22:56 +1200

    removed importing components from react

--- src/components/atoms/DisplayWebsite.jsx
+++ src/components/atoms/DisplayWebsite.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import brands from '@fortawesome/fontawesome-free-brands'
 import faGlobe from '@fortawesome/fontawesome-free-solid/faGlobe'
 
 function DisplayWebsite (props) {
