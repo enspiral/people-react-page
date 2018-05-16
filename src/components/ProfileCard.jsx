50c3f78a14207db1231db362dc7626ff054eb6c2
dazyang <dazyang@protonmail.com>
2018-05-16 15:37:46 +1200

    Created randomiseCardBk function added extra backgrounds for rendering

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -7,10 +7,14 @@ import InfoDescription from './atoms/InfoDescription'
 class ProfileCard extends Component {
   render(){
     const {person} = this.props
+    function randomiseCardBK () {
+      const index = Math.floor(Math.random() * 4)
+      return `info-wrapper-${index + 1}`
+    }
     return (
       <div className='profile-cards'>
         <ProfileImage image={person.gravatarUrl} />
-        <div className='info-wrapper'>
+        <div className={randomiseCardBK()}>
           <div className='info-container'>
             <InfoHeader person={person}/>
             <InfoDescription description={person.publicDescription}/>
