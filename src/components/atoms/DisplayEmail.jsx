74a976751c1c3957feefafe94f1adcbd39de2b3c
Daniel Lewis <dan@rootsystems.nz>
2018-05-18 23:28:01 +1200

    tidy up formatting and update FA size

--- src/components/atoms/DisplayEmail.jsx
+++ src/components/atoms/DisplayEmail.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import envelope from '@fortawesome/fontawesome-free-solid/faEnvelope'
 
-function DisplayEmail(props) {
+function DisplayEmail (props) {
   const { email } = props
   return (
     <div className='social-icons'>
-      <a href={"mailto:" + email}><FontAwesomeIcon icon={envelope} size='1' color='darkgrey'/></a>
+      <a href={'mailto:' + email}><FontAwesomeIcon icon={envelope} size='1x' color='darkgrey' /></a>
     </div>
-  );
+  )
 }
 
-export default DisplayEmail;
+export default DisplayEmail
--- src/components/atoms/DisplayWebsite.jsx
+++ src/components/atoms/DisplayWebsite.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faGlobe from '@fortawesome/fontawesome-free-solid/faGlobe'
 
 function DisplayWebsite (props) {
-  const {website} = props  
+  const {website} = props
   return (
     <div className='social-icons'>
-      <a href={website} target="_blank"><FontAwesomeIcon icon={faGlobe} size='1' color='darkgrey'/></a>
+      <a href={website} target='_blank'><FontAwesomeIcon icon={faGlobe} size='1x' color='darkgrey' /></a>
     </div>
-    );
+  )
 }
 
-export default DisplayWebsite;
+export default DisplayWebsite
