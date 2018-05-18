e8aa4f2ccdc2e828d8e5dcea7ad1acce65ffb492
Daniel Lewis <dan@rootsystems.nz>
2018-05-18 22:46:51 +1200

    tidy up syntax and chage FA size from 1 to 1x as per docs

--- src/components/atoms/DisplayLinkedIn.jsx
+++ src/components/atoms/DisplayLinkedIn.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import brands from '@fortawesome/fontawesome-free-brands'
 
 function DisplayLinkedIn (props) {
   const {linkedin} = props
-    return (
-      <div className='social-icons'>
-        <a href={linkedin} target="_blank"><FontAwesomeIcon icon={["fab", "linkedin"]} size='1' color='darkgrey'/></a>
-      </div>
-    );
+  return (
+    <div className='social-icons'>
+      <a href={linkedin} target='_blank'><FontAwesomeIcon icon={['fab', 'linkedin']} size='1x' color='darkgrey' /></a>
+    </div>
+  )
 }
 
-export default DisplayLinkedIn;
+export default DisplayLinkedIn
--- src/components/atoms/DisplayTwitter.jsx
+++ src/components/atoms/DisplayTwitter.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import brands from '@fortawesome/fontawesome-free-brands'
 
-
 function DisplayTwitter (props) {
   const {twitter} = props
-    return (
-      <div className='social-icons'>
-        <a href={twitter} target="_blank"><FontAwesomeIcon icon={["fab", "twitter"]} size='1' color='darkgrey'/></a>
-      </div>
-    );
+  return (
+    <div className='social-icons'>
+      <a href={twitter} target='_blank'><FontAwesomeIcon icon={['fab', 'twitter']} size='1x' color='darkgrey' /></a>
+    </div>
+  )
 }
 
-export default DisplayTwitter;
+export default DisplayTwitter
