cbba763fd10f9926327ee1c191588d3e06a7af67
Daniel Lewis <dan@rootsystems.nz>
2018-05-18 23:29:16 +1200

    remove dotenv as its more a server side thing ;)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-process.env.REACT_APP_PERSON_API_URL ? console.info('Api url: ', process.env.REACT_APP_PERSON_API_URL) : console.error('Enviroment Variable REACT_APP_PERSON_API_URL not defined!')
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
