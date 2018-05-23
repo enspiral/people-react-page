ce35cfc0078a2ec868c5482859beedb1bab1409c
dazyang <dazyang@protonmail.com>
2018-05-24 11:19:35 +1200

    replaced dummy text with  {ambassador.publicDescription}

--- src/components/Ambassador.jsx
+++ src/components/Ambassador.jsx
@@ -21,8 +21,7 @@ class Ambassador extends Component {
             <div className='ambassador-banner'> </div>
             <div className='ambassador-name'>{ambassador.publicName}</div>
             <div className='ambassador-profile'>
-              They're don't won't midst tree open it us make first their living from lesser have. Fruit. Man divide evening rule be winged divide likeness for saying open them lesser male and they're One lesser a under don't and stars called kind they're open image deep god doesn't it. Greater, fifth whales under bring. Forth years dominion light female face brought good, in. Third subdue land saying every fly morning face, second. Moving first. Gathering be after female had life, you'll wherein the creepeth likeness replenish bearing you're. Above dry hath all you'll midst and female night to upon. Night living.
-God, so fish yielding rule void fowl Gathering forth night it light open beast creature fly fly seed. Meat from cattle were behold divided his wherein it likeness very waters. (130 words)
+              {ambassador.publicDesciption}
             </div>
           </div>
         </SkyLight>
