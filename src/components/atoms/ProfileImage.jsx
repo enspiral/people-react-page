fa396c1df4e4d53db42a8c47e3dbe1d5eb4c8ff0
Daniel Lewis <dan@rootsystems.nz>
2018-06-29 18:33:13 +1200

    set default image to robot

--- src/components/atoms/ProfileImage.jsx
+++ src/components/atoms/ProfileImage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function ProfileImage (props) {
-    console.log(props.image)
-    return (
-        <div className='profile-image'>
-          <img src={`${props.image}?s=400`} alt='name' width='100%'/>
-        </div>
-    )
-  }
+  console.log(props.image)
+  return (
+      <div className='profile-image'>
+        <img src={`${props.image}?s=400&d=robohash`} alt='name' width='100%'/>
+      </div>
+  )
+}
 
- export default ProfileImage;    
+export default ProfileImage;
