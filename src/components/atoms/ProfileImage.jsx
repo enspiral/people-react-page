e3c113724800f3ae03ee0f6d82c170592d8585aa
Dan <dan.lewis@bamboocreative.nz>
2020-05-08 16:00:48 +1200

    update gravatar size

--- src/components/atoms/ProfileImage.jsx
+++ src/components/atoms/ProfileImage.jsx
@@ -4,7 +4,7 @@ function ProfileImage (props) {
     console.log(props.image)
     return (
         <div className='profile-image'>
-          <img src={`${props.image}?s=400`} alt='name' width='100%'/>
+          <img src={`${props.image}&s=600`} alt='name' width='100%'/>
         </div>
     )
   }
