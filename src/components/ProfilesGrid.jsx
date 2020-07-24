fe3de87d1d25fe8ba8eac588285c507bb7bc6cd5
Dan <dan.lewis@bamboocreative.nz>
2020-07-25 05:09:46 +1200

    update cards with width 100%

--- src/components/ProfilesGrid.jsx
+++ src/components/ProfilesGrid.jsx
@@ -20,7 +20,7 @@ function ProfilesGrid(props) {
       <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,700&display=swap" rel="stylesheet"></link>
       <h1 className='community-titles'><span style={{color:"#03A095"}}>Contributors</span> and <span style={{color:"#805298"}}>Membors</span></h1>
       <Masonry
-        className={'my-gallery-class'}
+        className={'cards'}
         style={style}
         elementType={'ul'}
         options={masonryOptions}
