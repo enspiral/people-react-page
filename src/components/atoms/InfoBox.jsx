8dbc17981a116c2a66907f26990b15e314e4ef5f
Dan <dan.lewis@bamboocreative.nz>
2020-07-24 12:39:28 +1200

    rm random background

--- src/components/atoms/InfoBox.jsx
+++ src/components/atoms/InfoBox.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 
 export function InfoBox (props) {
-  function randomiseCardBK () {
-    const index = Math.floor(Math.random() * 4)
-    return `info-wrapper-${index + 1}`
-  }
   return (
-    <div className={randomiseCardBK()}>
-      <div className='info-container'>
-        {props.children}
-      </div>
+    <div className='info-container'>
+      {props.children}
     </div>
   )
 }
