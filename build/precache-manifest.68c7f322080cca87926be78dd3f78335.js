7f4aa2b584073ea0389789a5c3f46b013cf112f0
Dan <dan.lewis@bamboocreative.nz>
2020-08-07 08:49:35 +1200

    include latest build file

---
+++ build/precache-manifest.68c7f322080cca87926be78dd3f78335.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2002118347ed74117deb",
+    "url": "/static/css/main.1b85b63c.chunk.css"
+  },
+  {
+    "revision": "2002118347ed74117deb",
+    "url": "/static/js/main.78ded7e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d60cc802cf0745c610d0",
+    "url": "/static/js/2.8f874c5b.chunk.js"
+  },
+  {
+    "revision": "120b9869f24051b08188a3ea52c615e2",
+    "url": "/index.html"
+  }
+];
